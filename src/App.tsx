35fb62b0a9a7253595456690f884079dac06c1fd
WagnerWD40 <wagnerwd40@gmail.com>
2021-01-23 21:46:00 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+    View,
+    Text,
+    SafeAreaView,
+    StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+            <View>
+                <Text>
+                    Hellow world
+                </Text>
+            </View>
+        </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
